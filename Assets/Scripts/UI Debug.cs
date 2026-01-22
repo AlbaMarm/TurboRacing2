ef9dc59814d335fcb277c47ae4beb67e96e2112e
jcamloz <jcamloz553@g.educaand.es>
2026-01-22 19:30:00 +0100

    Commit Inicial

---
+++ Assets/Scripts/UI Debug.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDebug : MonoBehaviour
+{
+    [SerializeField] private Text steering;
+    [SerializeField] private CarController car;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+}

96f80f38c05c4b7918b9afc7a66bca257fe6c4ea
AlbaMarm <alba03943@gmail.com>
2026-01-27 21:08:05 +0100

    pruebecita

--- Assets/Scripts/Multi/InicioController.cs
+++ Assets/Scripts/Multi/InicioController.cs
@@ -1,171 +1,220 @@
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System;
 using Fusion;
 using Fusion.Sockets;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class InicioController : MonoBehaviour, INetworkRunnerCallbacks
 {
     private NetworkRunner red;
-    private ListaJugadoresController LJC;
     private NetworkSceneManagerDefault sceneManager;
 
+    // One session → one player list
+    private readonly Dictionary<PlayerRef, NetworkObject> players = new();
 
     private void Awake()
     {
         red = FindAnyObjectByType<NetworkRunner>();
-        red.name = "TurboRacing";
-        red.ProvideInput = true;
-        red.AddCallbacks(this);
         sceneManager = FindAnyObjectByType<NetworkSceneManagerDefault>();
 
-        LJC = FindAnyObjectByType<ListaJugadoresController>();
+        red.name = "TurboRacing";
+        red.ProvideInput = true;
+
+        // Prevent double registration
+        red.RemoveCallbacks(this);
+        red.AddCallbacks(this);
     }
+
+    // ============================
+    // CONNECTION
+    // ============================
 
     public async void Conectar()
     {
-        Dictionary<string, SessionProperty> propiedades = new Dictionary<string, SessionProperty>();
-        propiedades.Add("Ganador", (SessionProperty)0);
+        var propiedades = new Dictionary<string, SessionProperty>
+        {
+            { "Ganador", (SessionProperty)0 }
+        };
 
-        StartGameArgs argumentos = new StartGameArgs();
-        argumentos.GameMode = GameMode.AutoHostOrClient;
-        argumentos.SessionName = "Carrera";
-        argumentos.PlayerCount = 2;
-        argumentos.SceneManager = sceneManager;
-        argumentos.Scene = SceneRef.FromIndex(SceneManager.GetActiveScene().buildIndex);
-        argumentos.SessionProperties = propiedades;
+        var args = new StartGameArgs
+        {
+            GameMode = GameMode.AutoHostOrClient,
+            SessionName = "Carrera",
+            PlayerCount = 2,
+            SceneManager = sceneManager,
+            Scene = SceneRef.FromIndex(SceneManager.GetActiveScene().buildIndex),
+            SessionProperties = propiedades
+        };
 
-        await red.StartGame(argumentos);
-
-    }
-    public void OnConnectedToServer(NetworkRunner runner)
-    {
-
-    }
-
-    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
-    {
-
+        await red.StartGame(args);
     }
 
-    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
-    {
-
-    }
-
-    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
-    {
-
-    }
-
-    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
-    {
-
-    }
-
-    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
-    {
-
-    }
-
-    public void OnInput(NetworkRunner runner, NetworkInput input)
-    {
-
-    }
-
-    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
-    {
-
-    }
-
-    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
-    {
-
-    }
-
-    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
-    {
-
-    }
+    // ============================
+    // CALLBACKS
+    // ============================
 
     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
     {
-        Debug.Log("Se conecta");
+        if (!runner.IsServer)
+            return;
 
-        if (runner.IsServer)
-        {
-            if (!LJC.listaSJ.ContainsKey(runner.SessionInfo))
-            {
-                LJC.listaSJ.Add(runner.SessionInfo, new Dictionary<PlayerRef, NetworkObject>());
-            }
+        // SAFE: overwrites if already exists
+        players[player] = null;
 
-            LJC.listaSJ[runner.SessionInfo].Add(player, null);
-
-            if (runner.IsSceneAuthority)
-            {
-                if (runner.SessionInfo.PlayerCount == runner.SessionInfo.MaxPlayers)
-                {
-                    runner.UnloadScene(SceneRef.FromIndex(0));
-                    runner.LoadScene(SceneRef.FromIndex(3));
-                }
-                else
-                {
-                    runner.UnloadScene(SceneRef.FromIndex(0));
-                    runner.LoadScene(SceneRef.FromIndex(1));
-                }
-            }
-        }
+        Debug.Log($"Player joined: {player}");
     }
 
     public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
     {
-        if (runner.IsServer && LJC.listaSJ[runner.SessionInfo].TryGetValue(player, out NetworkObject p))
-        {
-            if (p != null)
-            {
-                runner.Despawn(p);
-            }
+        if (!runner.IsServer)
+            return;
 
-            LJC.listaSJ[runner.SessionInfo].Remove(player);
-        }
-    }
+        players.Remove(player);
 
-    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
-    {
-
-    }
-
-    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
-    {
-
+        Debug.Log($"Player left: {player}");
     }
 
     public void OnSceneLoadDone(NetworkRunner runner)
     {
+        if (!runner.IsSceneAuthority)
+            return;
 
+        // Decide scene ONLY after scene load
+        if (runner.SessionInfo.PlayerCount == runner.SessionInfo.MaxPlayers)
+        {
+            runner.LoadScene(SceneRef.FromIndex(3)); // Game scene
+        }
+        else
+        {
+            runner.LoadScene(SceneRef.FromIndex(1)); // Lobby / Waiting
+        }
     }
 
-    public void OnSceneLoadStart(NetworkRunner runner)
+    public void OnDisconnectedFromServer(NetworkRunner runner)
     {
-
+        Debug.Log("Disconnected from server");
+        HandleDisconnectAsync(runner);
     }
 
-    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    private async void HandleDisconnectAsync(NetworkRunner runner)
     {
-        
+        await runner.Shutdown();
+        SceneManager.LoadScene(0); // Main menu
     }
 
-    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+    // ============================
+    // UNUSED CALLBACKS (REQUIRED)
+    // ============================
+
+    public void OnConnectedToServer(NetworkRunner runner) { }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+    public void OnInput(NetworkRunner runner, NetworkInput input) { }
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) { }
+    public void OnSceneLoadStart(NetworkRunner runner) { }
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+
+    void INetworkRunnerCallbacks.OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
     {
-
+        throw new NotImplementedException();
     }
 
-    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+    void INetworkRunnerCallbacks.OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
     {
-
+        throw new NotImplementedException();
     }
 
+    void INetworkRunnerCallbacks.OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnConnectedToServer(NetworkRunner runner)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnSceneLoadDone(NetworkRunner runner)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnSceneLoadStart(NetworkRunner runner)
+    {
+        throw new NotImplementedException();
+    }
 }
